220f538f4bbaf52bca5add6ee6ead9a34730e113
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-13 14:56:32 +0200

    Created email structure

---
+++ mail/js/script.js
@@ -0,0 +1,23 @@
+//chiedo all'utente l'email
+let emailUser = prompt('Inserisci la tua e-mail');
+
+// lista di email che possono accedere
+const emailList = ['mario.rossi@gmail.com', 'marco.bianchi@gmail.com', 'giuseppe.verdi@gmail.com'];
+
+let emailAbsent = true; 
+
+//controllo se emailUser è presente in emailList
+for (let i = 0; i < emailList.length; i++){
+    const currentEmail = emailList[i];
+
+    if (emailUser === currentEmail){
+        emailAbsent = false;
+    } 
+}
+
+//messaggio di accesso negato o consentito
+if (emailAbsent){
+    alert('La email non è presente nella lista di chi può accedere');
+} else {
+    alert('Puoi accedere')
+}
